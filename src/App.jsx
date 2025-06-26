133e36d86082749d95262a692cd4e41cdf4338f3
Rifat Jayed <rifat.jayed2000@gmail.com>
2025-06-26 13:01:58 +0600

    about us page add

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,65 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Description from "./components/pages/description/Description"
-import Home from "./components/pages/home/Home"
-import JobSearch from "./components/pages/jobSearch/JobSearch"
-import Navbar from "./components/shared/navbar/Navbar"
+import Description from "./components/pages/description/Description";
+import Home from "./components/pages/home/Home";
+import JobSearch from "./components/pages/jobSearch/JobSearch";
+import Navbar from "./components/shared/navbar/Navbar";
+import About from "./components/pages/about/About";
 
 function App() {
- 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:  <>
-    <Navbar></Navbar>
-    <Home></Home>
-   ,
-    </>,
-  },
-   {
-    path: "/job",
-    element: <>
-    <Navbar></Navbar>
-    <JobSearch></JobSearch> ,
-    </>
-    
-    
-  },
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Navbar></Navbar>
+          <Home></Home>,
+        </>
+      ),
+    },
+    {
+      path: "/about",
+      element: (
+        <>
+          <Navbar></Navbar>
+          <About></About>,
+        </>
+      ),
+    },
+    {
+      path: "/job",
+      element: (
+        <>
+          <Navbar></Navbar>
+          <JobSearch></JobSearch> ,
+        </>
+      ),
+    },
 
-   {
-    path: "/jobDescrption",
-    
-    element: 
-    <>
-     <Navbar></Navbar>
-    <Description></Description>,
-    
-    </>
-    
-  },
+    {
+      path: "/jobDescrption",
 
-]);
+      element: (
+        <>
+          <Navbar></Navbar>
+          <Description></Description>,
+        </>
+      ),
+    },
+  ]);
   return (
-   
-
-//       <div className="">
-//          {/* <div className="flex flex-col items-center gap-20"></div> */}
-//  {/* <Home></Home> */}
-//  <Navbar></Navbar>
-//  {/* <JobSearch></JobSearch> */}
-//  <Description></Description>
-//     </div> 
-   <div>
+    //       <div className="">
+    //          {/* <div className="flex flex-col items-center gap-20"></div> */}
+    //  {/* <Home></Home> */}
+    //  <Navbar></Navbar>
+    //  {/* <JobSearch></JobSearch> */}
+    //  <Description></Description>
+    //     </div>
+    <div>
       <main className="overflow-x-hidden">
         <RouterProvider router={router} />
       </main>
     </div>
-   
-  )
+  );
 }
 
-export default App
+export default App;
